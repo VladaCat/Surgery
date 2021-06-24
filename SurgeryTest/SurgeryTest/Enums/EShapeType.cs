8d2403d31a52220eb9b44964c264ed0a6e37290a
Vlada Dorozhko <45641162+VladaCat@users.noreply.github.com>
2021-06-24 18:38:19 +0300

    Json logic in new project test

---
+++ SurgeryTest/SurgeryTest/Enums/EShapeType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team_Project_Paint.PaintEnum
+{
+    public enum EShapeType
+    {
+        Dot,
+        Rect,
+        Ellipse,
+        Curve,
+        Line,
+        Triangle,
+        Select,
+        Hexagon,
+        RoundingRect
+    }
+        
+}
